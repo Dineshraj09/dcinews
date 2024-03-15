927ed3ee214c9863107551b8c90a7eddea7bafdb
Dineshraj09 <dineshpathmaraj@gmail.com>
2024-03-15 12:59:32 +0100

    new branch commit

---
+++ articles/admin.py
@@ -0,0 +1,18 @@
+# articles/admin.py
+from django.contrib import admin
+from .models import Article, Comment
+
+
+class CommentInline(admin.StackedInline):  # also possible choice - TabularInline
+    model = Comment
+    extra = 0
+
+
+class ArticleAdmin(admin.ModelAdmin):
+    inlines = [
+        CommentInline,
+    ]
+
+
+admin.site.register(Article, ArticleAdmin)
+admin.site.register(Comment)
